0e984112a9788b15ffbb741125dafdcb54315dd7
Clesio Nascimento <clesio.nascto@gmail.com>
2023-12-04 03:55:08 -0300

    microsserviços de pedido

---
+++ pedido/src/modules/create-order/create-order.usecase.ts
@@ -0,0 +1,31 @@
+import { prismaClient } from "../../infra/database/prismaClient"
+
+type CreateOrderRequest = {
+    customerId: string
+    items: [{ productId: string, quantity: number }]
+}
+
+export class CreateOrderUseCase {
+    constructo() { }
+
+    // Requisição para API de produtos para verificar se tem estoque do produto
+    // axios.get('/product')
+
+    async execute(data: CreateOrderRequest) {
+        const order = await prismaClient.order.create({
+            data: {
+                CustomerId: data.customerId,                
+                status: "AGUARDANDO PAGAMENTO",
+                OrderItems: {
+                    createMany: {
+                        data: data.items
+                    },
+                },
+
+            },
+
+        })     
+
+        return order
+    }
+}
--- pedido/src/server.ts
+++ pedido/src/server.ts
@@ -1,9 +1,12 @@
 import express from "express"
 import "./infra/providers/kafka/consumers"
+import { router } from "./infra/routes";
 
 const app = express();
 app.use(express.json());
 
 const PORT = process.env.PORT ?? 3002;
+
+app.use(router)
 
 app.listen(PORT, () => console.log(`Server client is running on PORT ${PORT}`))
